Modify the wrong get_information function in ann_network.py. Extract the accuracy function from ann_train.py to utils.py. Add the data based and model based normalization methods in max_norm.py

ann_network.py
@@ -1,4 +1,5 @@
 from torch import nn
+from collections import OrderedDict
 class ANN(nn.Module):
  def __init__(self,bias=False):
   super(ANN,self).__init__()
@@ -11,10 +12,16 @@ class ANN(nn.Module):
    nn.AvgPool2d(kernel_size=2, stride=2),
   )
   self.classifier=nn.Linear(7*7*64,10,bias=bias)
+  self.module2name=self.get_information()
 
  def forward(self,input):
   output=self.feature(input)
   output=output.view(input.shape[0],-1)
   output=self.classifier(output)
   return output
-
+ def get_information(self):
+  module2name=OrderedDict()
+  for name,module in self.named_modules():
+   if isinstance(module,(nn.Conv2d,nn.Linear)):
+    module2name[module]=name
+  return module2name

ann_train.py
@@ -7,20 +7,7 @@ from torch import nn
 from __init__ import *
 from collections import OrderedDict
 from copy import deepcopy
-
-
-def accuracy(model,dataloader):
-    with torch.no_grad():
-        model.eval()
-        correct_num=0
-        total_num=0
-        for i,(batch_x,batch_y) in enumerate(dataloader):
-            batch_x=batch_x.cuda()
-            batch_y=batch_y.cuda()
-            output=model(batch_x)
-            correct_num+=(output.argmax(dim=1)==batch_y).sum().item()
-            total_num+=batch_x.shape[0]
-    return correct_num/total_num
+from utils import accuracy
 
 seed=0
 torch.manual_seed(seed) # 为CPU设置随机种子